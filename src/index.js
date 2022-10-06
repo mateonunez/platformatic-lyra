c28a84465e99b8b879abb25181b31c508865c4c7
mateonunez <mateonunez95@gmail.com>
2022-10-06 10:35:55 +0200

    chore!(lru-cache): play with cache

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ const Fastify = require('fastify')
 const mapper = require('@platformatic/sql-mapper')
 const FastifyLyra = require('@mateonunez/fastify-lyra')
 const resolveSchema = require('./lib/schema.js')
-
 const connectionString = 'sqlite://./quotes.sqlite'
+const LRU = require('lru-cache')
 
 async function main () {
   const app = Fastify({
@@ -15,12 +15,18 @@ async function main () {
   })
 
   const conn = await mapper.connect({ connectionString })
+  const cache = new LRU({ max: 34000 })
+  let ids = []
 
   app.ready(async () => {
     const quotes = await conn.entities.quote?.find()
     if (quotes?.length > 0) {
       for (const quote of quotes) {
-        if (quote.id) delete quote.id
+        if (quote.id) {
+          const { id } = quote 
+          delete quote.id
+          cache.set(id, quote)
+        }
         await app.lyra.insert(quote)
       }
     }
@@ -32,12 +38,29 @@ async function main () {
 
   app.get('/quotes/lyra/:author', async function (req, reply) {
     const { author } = req.params
-    const search = await app.lyra.search({
-      term: author,
-      exact: true,
-      limit: 1000
-    })
-    return { count: search.hits.length, quotes: search.hits }
+
+    let hits = []
+    if (!ids.length || cache.get(author)) {
+      const search = await app.lyra.search({
+        term: author,
+        exact: true,
+        limit: 34000
+      })
+      hits = search.hits.map(hit => {
+        ids.push(hit.id)
+        return {
+          quote: hit.quote,
+          author: hit.author
+        }
+      })
+    } else {
+      const keys = [...cache.keys()]
+      for (const key of keys) {
+        hits.push(cache.get(key))
+      }
+    }
+
+    return { count: hits.length, quotes: hits }
   })
 
   app.get('/quotes/mapper/:author', async function (req, reply) {
